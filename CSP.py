ad44540a6cbc4c0505d02b39e5077f70f52adb9f
ThoryaAhmed <thoryaabdelzaher@gmail.com>
2022-06-05 05:07:29 +0200

    CSP different algorithms

---
+++ CSP.py
@@ -0,0 +1,139 @@
+class CSP ():
+    def __init__(self, variables, domains, neighbors, constraints):
+
+        variables = variables or list(domains.keys())
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def choices(self, var):
+        return (self.curr_domains or self.domains)[var]   
+
+    def conflicts(self, var, val, assignment):
+        # Subclasses may implement this more efficiently
+        def conflict(var2):
+            return (var2 in assignment and
+                    not self.constraints(var, val, var2, assignment[var2]))
+        #return count(conflict(v) for v in self.neighbors[var])         
+        for v in self.neighbors:
+            if(conflict(v)):
+                return True
+        return False  
+
+    def assign(self, var, val, assignment):
+        """Add {var: val} to assignment; Discard the old value if any."""
+        assignment[var] = val
+        self.nassigns += 1          
+
+    
+    def support_pruning(self):
+        """Make sure we can prune values from domains. (We want to pay
+        for this only if we use it.)"""
+        if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def suppose(self, var, value):
+        """Start accumulating inferences from assuming var=value."""
+        self.support_pruning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        self.curr_domains[var] = [value]
+        return removals    
+
+    def restore(self, removals):
+        """Undo a supposition and all inferences from it."""
+        for B, b in removals:
+            self.curr_domains[B].append(b)    
+
+    def unassign(self, var, assignment):
+        if var in assignment:
+            del assignment[var]        
+
+    def prune(self, var, value, removals):
+        """Rule out var=value."""
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var, value))  
+
+    def goal_test(self, state):
+        """The goal is to assign all variables, with all constraints satisfied."""
+        assignment = dict(state)
+        return (len(assignment) == len(self.variables)
+                and all(self.conflicts(variables, assignment[variables], assignment) == False
+                        for variables in self.variables))  
+
+def select_unassigned_variable(assignment, csp):
+    for var in csp.variables :
+        if var not in assignment:
+            return var
+    return None
+
+
+
+def order_domain_values(var, csp):
+    return csp.choices(var)
+
+def no_inference(csp, var, value, assignment, removals):
+    return True
+
+def backtracking_search(csp,inference=no_inference):
+
+    def backtrack(assignment):
+        if len(assignment) == len(csp.variables):
+            return assignment
+        var = select_unassigned_variable(assignment, csp)
+        for value in order_domain_values(var, csp):
+            if csp.conflicts(var, value, assignment) == False:
+                csp.assign(var, value, assignment)
+                removals = csp.suppose(var, value)
+                if inference(csp, var, value, assignment, removals):
+                    result = backtrack(assignment)
+                    if result is not None:
+                        return result
+                csp.restore(removals)
+        csp.unassign(var, assignment)
+        return None
+
+    result = backtrack({})
+    assert result is None or csp.goal_test(result)
+    return result   
+
+def forward_checking(csp, var, value, assignment, removals):
+    """Prune neighbor values inconsistent with var=value."""
+    csp.support_pruning()
+    for B in csp.neighbors[var]:
+        if B not in assignment:
+            for b in csp.curr_domains[B][:]:
+                if not csp.constraints(var, value, B, b):
+                    csp.prune(B, b, removals)
+            if not csp.curr_domains[B]:
+                return False
+    return True
+
+
+
+def remove_incosistant_values(csp, Xi, Xj, removals):
+    """Return true if we remove a value."""
+    removed = False
+    for x in csp.curr_domains[Xi][:]:
+        if all(not csp.constraints(Xi, x, Xj, y) for y in csp.curr_domains[Xj]):
+            csp.prune(Xi, x, removals)
+            removed = True
+    return removed
+
+
+def AC3(csp, var, value, assignment, removals):
+    """[Figure 6.3]"""
+    queue = [(Xi, Xk) for Xi in csp.variables for Xk in csp.neighbors[Xi]]
+    csp.support_pruning()
+    while queue:
+        (Xi, Xj) = queue.pop()
+        if remove_incosistant_values(csp, Xi, Xj, removals):
+            if not csp.curr_domains[Xi]:
+                return False
+            for Xk in csp.neighbors[Xi]:
+                if Xk != Xj:
+                    queue.append((Xk, Xi))
+    return True
